d9077ba36072b8c73a2ac89dc3c8ee33af626bad
Maxime <maxime.boulle@efrei.net>
2024-04-08 18:44:35 +0200

    modified:   my-app/src/app/app.component.html 	modified:   my-app/src/app/app.component.ts 	modified:   my-app/src/app/app.routes.ts 	deleted:    my-app/src/app/footer/footer.component.css 	deleted:    my-app/src/app/footer/footer.component.html 	deleted:    my-app/src/app/footer/footer.component.spec.ts 	deleted:    my-app/src/app/footer/footer.component.ts 	modified:   my-app/src/app/home/home.component.html 	modified:   my-app/src/app/order/order.component.html

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { OrderComponent } from './order/order.component';
-import { FooterComponent } from './footer/footer.component';
+
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,7 @@ import { FooterComponent } from './footer/footer.component';
     RouterOutlet,
     HomeComponent,
     OrderComponent,
-    FooterComponent,
+
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
